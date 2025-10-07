1bc25eb54280dfdfcb08ac3a73b57958ca32be97
Risolat <risolat2803@gmail.com>
2025-10-07 11:22:18 +0500

    week1

---
+++ concert_ticket.py
@@ -0,0 +1,20 @@
+AN=input('Artist/band name: ')
+TN=input('Tour name: ')
+CT=input('City name: ')
+DT=input("Date and time: ")
+THN=input("Your full name: ")
+print("""
+********************************************
+                    --ADMIT ONE: VIP PASS
+********************************************
+                                           """)
+print("Artist:   ", AN)
+print("Tour:     ", TN)
+print("Location: ", CT)
+print("Date:     ", DT)
+print("--------------------------------------------------")
+print(f'Issued to:     {THN}')
+print(f"Event Code:    {AN}-{CT}-VIP")
+print("""--------------------------------------------------
+                     * Enjoy the Show!*  
+********************************************""")
